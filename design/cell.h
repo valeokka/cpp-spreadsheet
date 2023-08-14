90039039cae10bdcd9f3fca9f146fbdd3dfb8511
valeokka <swekarochi@gmail.com>
2023-08-14 22:01:00 +0300

    Added design

--- design/cell.h
+++ design/cell.h
@@ -5,7 +5,7 @@
 
 class Cell : public CellInterface {
 public:
-    Cell();
+    Cell(SheetInterface& sheet_);
     ~Cell();
 
     void Set(std::string text);
@@ -13,11 +13,54 @@ public:
 
     Value GetValue() const override;
     std::string GetText() const override;
+    
 
 private:
     class Impl;
-    class EmptyImpl;
-    class TextImpl;
-    class FormulaImpl;
+
     std::unique_ptr<Impl> impl_;
+    SheetInterface& sheet_;
+    bool IsEdited;
+    CellInterface::Value cached_value_;
+
+    class Impl{
+    public:
+        explicit Impl(const std::string raw);
+        virtual Value GetValue() = 0;
+        virtual std::string GetText() = 0;
+        virtual GetReference() = 0;
+    protected:
+        std::string raw_text_;
+        std::vector<Position> parents_;
+        std::vector<Position> childs_;
+    };
+
+    class EmptyImpl final : public Impl{
+    public:
+        explicit EmptyImpl(const std::string raw);
+        Value GetValue() override;
+        std::string GetText() override;
+    };
+
+    class TextImpl : public Impl{
+    public:
+        explicit TextImpl(const std::string raw);
+        Value GetValue() override;
+        std::string GetText() override;
+    }
+
+    class FormulaImpl : public Impl{
+    public:
+        explicit FormulaImpl(const std::string& raw, SheetInterface& sheet_);
+        Value GetValue() override;
+        std::string GetText() override;
+        std::forward_list<Position> GetReferencedCells() const;  
+        void InvalidateCache();      
+
+    private:
+        std::unique_ptr<FormulaInterface> formula_;
+        std::optional<CellInterface::Value> cached_value_;
+        SheetInterface& sheet_;
+    };
+
 };
--- design/sheet.h
+++ design/sheet.h
@@ -4,31 +4,41 @@
 #include "common.h"
 
 #include <functional>
+#include <unordered_map>
+#include <cmath>
+#include <vector>
+
+using Cell_ptr = std::unique_ptr<Cell>;
+using Cols = std::unordered_map<int, Cell_ptr>;
 
 class Sheet : public SheetInterface {
 public:
+    Sheet();
     ~Sheet();
 
     void SetCell(Position pos, std::string text) override;
-
     const CellInterface* GetCell(Position pos) const override;
     CellInterface* GetCell(Position pos) override;
-
     void ClearCell(Position pos) override;
-
     Size GetPrintableSize() const override;
-
     void PrintValues(std::ostream& output) const override;
     void PrintTexts(std::ostream& output) const override;
 
-    const Cell* GetConcreteCell(Position pos) const;
-    Cell* GetConcreteCell(Position pos);
-
 private:
-    void MaybeIncreaseSizeToIncludePosition(Position pos);
-    void PrintCells(std::ostream& output,
-                    const std::function<void(const CellInterface&)>& printCell) const;
-    Size GetActualSize() const;
+    std::unordered_map<Position, Cell_ptr, PositionHash> cells_;
+    mutable Size printable_size_;
+    mutable std::vector<int> rows_count_;
+    mutable std::vector<int> cols_count_;
+    
+    struct CellValuePrinter {
+        std::string operator()(double value) const;
+        std::string operator()(const std::string& value) const;
+        std::string operator()(const FormulaError& value) const;
+    };
 
-    std::vector<std::vector<std::unique_ptr<Cell>>> cells_;
+    void InvalidateCacheChilds(Position pos);
+    void ChechCyclicDependance()(Position pos);         
+    bool IsValid(Position pos) const;
+    void RefreshPrintableSize() const;
+    void TestPosition(Position pos) const;
 };
